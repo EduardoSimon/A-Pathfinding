18c6529b67d7d83a4cdc74e947199e99a0974ac0
EduardoSimon <al341513@uji.es>
2016-11-29 18:46:15 +0100

    Project Added

---
+++ Assets/Scripts/Grid.cs
@@ -0,0 +1,106 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+
+public class Grid : MonoBehaviour {
+
+    #region FIELDS
+    public Transform player;
+    public Vector2 gridWorldSize;
+    public float nodeRadius;
+    Node[,] grid;
+    public LayerMask unwalkableMask;
+
+    float nodeDiameter;
+    int gridSizeX, gridSizeY;
+
+    #endregion
+
+    private void Start()
+    {
+        nodeDiameter = nodeRadius * 2;
+        gridSizeX = Mathf.RoundToInt(gridWorldSize.x / nodeDiameter);
+        gridSizeY = Mathf.RoundToInt(gridWorldSize.y / nodeDiameter);
+        CreateGrid();
+    }
+
+    void CreateGrid()
+    {
+        grid = new Node[gridSizeX, gridSizeY];
+        Vector3 worldBottomLeft = transform.position - Vector3.right * gridWorldSize.x / 2 - Vector3.forward * gridWorldSize.y / 2;
+        print(worldBottomLeft);
+
+        for (int i = 0; i < gridSizeX; i++)
+        {
+            for (int j = 0; j < gridSizeY; j++)
+            {
+                Vector3 worldPoint = worldBottomLeft + Vector3.right * (i * nodeDiameter + nodeRadius) + Vector3.forward * (j * nodeDiameter + nodeRadius);
+                bool walkable = !(Physics.CheckSphere(worldPoint, nodeRadius, unwalkableMask));
+                grid[i, j] = new Node(walkable, worldPoint,i, j);
+            }
+        }
+    }
+
+    public List<Node> GetNeighbours(Node node)
+    {
+        List<Node> neighbours = new List<Node>();
+
+        for (int i = -1; i <= 1; i++)
+        {
+            for (int j = -1; j <= 1; j++)
+            {
+                if (i == 0 && j == 0)
+                    continue;
+
+                int checkX = node.gridX + i;
+                int checkY = node.gridY + j;
+
+                if (checkX >= 0 && checkX < gridSizeX && checkY >= 0 && checkY < gridSizeY)
+                    neighbours.Add(grid[checkX, checkY]);
+            }
+        }
+
+        return neighbours;
+    }
+
+
+    public Node NodeFromWorldPoint(Vector3 worldPosition)
+    {
+        float percentX = (worldPosition.x + gridWorldSize.x / 2) / gridWorldSize.x;
+        float percentY = (worldPosition.z + gridWorldSize.y / 2) / gridWorldSize.y;
+        percentY = Mathf.Clamp01(percentY);
+        percentY = Mathf.Clamp01(percentY);
+
+        int x = Mathf.RoundToInt((gridSizeX - 1) * percentX); // we subtract one in order to get the index in 0 based form
+        int y = Mathf.RoundToInt((gridSizeY - 1) * percentY);
+        return grid[x, y];
+    }
+
+    public List<Node> path;
+
+    #region GIZMOS
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawWireCube(transform.position, new Vector3 (gridWorldSize.x, 0.1f, gridWorldSize.y));
+
+        if (grid != null)
+        {
+            Node playerNode = NodeFromWorldPoint(player.position);
+            foreach (Node nodo in grid)
+            {
+                Gizmos.color = (nodo.walkable) ? Color.white : Color.red;
+                if (path != null)
+                    if (path.Contains(nodo))
+                        Gizmos.color = Color.black;
+                if (playerNode == nodo) Gizmos.color = Color.cyan;
+                Gizmos.DrawCube(nodo.worldPosition, Vector3.one * (nodeDiameter - 0.1f));
+            }
+        }
+    }
+    #endregion
+
+
+
+}
---
+++ Assets/Scripts/Pathfinding.cs
@@ -0,0 +1,105 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Pathfinding : MonoBehaviour
+{
+    #region FIELDS
+    Grid grid;
+
+    public Transform source;
+    public Transform target;
+    #endregion
+
+    #region MONOBEHAVIOURS
+    private void Awake()
+    {
+        grid = GetComponent<Grid>();
+    }
+
+    private void Update()
+    {
+        FindPath(source.position, target.position);
+    }
+    #endregion
+
+    #region METHODS
+
+    void FindPath(Vector3 startPos, Vector3 targetPos)
+    {
+        Node startNode = grid.NodeFromWorldPoint(startPos);
+        Node targetNode = grid.NodeFromWorldPoint(targetPos);
+
+        List<Node> openSet = new List<Node>();
+        HashSet<Node> closedSet = new HashSet<Node>();
+
+        openSet.Add(startNode);
+
+        while(openSet.Count > 0)
+        {
+            Node current = openSet[0];
+
+            for (int i = 1; i < openSet.Count; i++)
+            {
+                if (openSet[i].fCost < current.fCost || openSet[i].fCost == current.fCost && openSet[i].hCost < current.hCost)
+                    current = openSet[i];
+            }
+
+            openSet.Remove(current);
+            closedSet.Add(current);
+
+            //if we found the target Node exit from the loop
+            if (current == targetNode)
+            {
+                RetracePath(startNode, targetNode);
+                return;
+            }
+
+            foreach (Node neighbour in grid.GetNeighbours(current))
+            {
+                if (!neighbour.walkable || closedSet.Contains(neighbour))
+                    continue;
+
+                int costToNeighbour = current.gCost + GetDistance(current, neighbour);
+                if(costToNeighbour < neighbour.gCost || !openSet.Contains(neighbour))
+                {
+                    neighbour.gCost = costToNeighbour;
+                    neighbour.hCost = GetDistance(neighbour, targetNode);
+                    neighbour.parent = current;
+
+                    if (!openSet.Contains(neighbour))
+                        openSet.Add(neighbour);
+                }
+            }
+
+        }
+    }
+
+    void RetracePath(Node start, Node end)
+    {
+        List<Node> path = new List<Node>();
+        Node currentNode = end;
+
+        while(currentNode != start)
+        {
+            path.Add(currentNode);
+            currentNode = currentNode.parent;
+        }
+
+        path.Reverse();
+        grid.path = path;
+    }
+
+    int GetDistance(Node A, Node B)
+    {
+        int distX = Mathf.Abs(A.gridX - B.gridX);
+        int distY = Mathf.Abs(A.gridY - B.gridY);
+
+        if (distX > distY)
+            return 14 * distY + 10 * (distX - distY);
+        else
+            return 14 * distX + 10 * (distY - distX);
+
+    }
+    #endregion
+}
